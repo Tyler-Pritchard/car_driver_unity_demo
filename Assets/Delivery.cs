da19cca25f64c4f6c3532d42cb661a2a84ad9008
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-07-05 18:15:19 -0700

    add object, camera follow

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class DeliveryService : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) 
     {
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -17,7 +17,7 @@ public class Driver : MonoBehaviour
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, steerAmount);
+        transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
 
     }
