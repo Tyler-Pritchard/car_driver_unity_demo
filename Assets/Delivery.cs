2e62c987c63f46e76eaed07e1eaf2835cb538136
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-07-05 20:29:35 -0700

    add obj triggers for packages, needs debug why functions not triggering

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeliveryService : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    float destroyDelay = 0.5f;
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Look out");   
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Witty message");
+        if (other.tag == "Package") {
+            Debug.Log("Package acquired");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
+        }
+        if (other.tag == "Customer" && hasPackage) {
+            Debug.Log("Package delivered");
+            hasPackage = false;
+        }
     }
 }
